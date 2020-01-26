28e2daf7432b48e3053a534393333eb2a0f97efe
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2020-01-26 11:38:57 +0000

    mongo new schema

--- Shiny/mongotry.R
+++ Shiny/mongotry.R
@@ -1,13 +1,13 @@
 library(mongolite)
 
-collection_name <- "responses"
+collection_name <- "health_responses"
 
 save_data <- function(data) {
   db <- mongo(collection = collection_name,
-              url = paste0("mongodb+srv://what:",
+              url = paste0("mongodb+srv://abc:",
                            options()$mongo_password,
-                           "@abc-xze3x.mongodb.net/test?retryWrites=true&w=majority")
-              )
+                           "@cluster0-hzl4d.mongodb.net/test?retryWrites=true&w=majority")
+  )
   data <- as.data.frame(t(data))
   db$insert(data)
 }
