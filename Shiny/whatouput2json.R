3347d67ddc9fcdcd514c9d23ac365b9564c7ec5b
mbytc1 <mbytc1@gmail.com>
2020-01-26 14:13:23 +0000

    Add files via upload
    fixed - sd change, json output in desired format

--- Shiny/whatouput2json.R
+++ Shiny/whatouput2json.R
@@ -24,12 +24,22 @@ db_clean <- db_clean %>% select(-V3) %>%
 
 
 # summarise if same website/V1 name
-db_out <- db_clean %>% mutate(V1=gsub("(^http://)|(^https://)|(/$)","",V1),
+db_sum <- db_clean %>% mutate(V1=gsub("(^http://)|(^https://)|(/$)","",V1),
                               V1=ifelse(!grepl("^www\\.",V1),paste0("www.",V1),V1)) %>%
-  group_by(V1) %>% summarise(V2=median(V2),n=n(),sd=sd(V2)) %>%
-  mutate(V2=V2*10,V2=ceiling(V2))
+  group_by(V1) %>% summarise(V2median=median(V2),n=n(),V2sd=sd(V2)) %>%
+  mutate(V2=V2median*10,V2median=ceiling(V2median)) %>% select(-V2median)
 
 #Output
+# alter to prep files in desired format
+db_out<-db_sum %>% select(V1,V2)
+n <- db_out$V1
+db_out <- as.data.frame(t(db_out[,-1]))
+colnames(db_out) <- n
+str(db_out) # Check the column types
+rownames(db_out) <- NULL
+
+# save to json
 library(jsonlite)
-toJSON(db_out %>% select(V1,V2), pretty=TRUE)
+toJSON(db_out,pretty=TRUE)
+
 
